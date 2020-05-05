bafbc9cf1a8872f42c0fb14ca47ac821b6a20f4e
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2020-05-05 16:32:46 -0600

    test format

--- vs-build/CreateGitVersion.bat
+++ vs-build/CreateGitVersion.bat
@@ -4,6 +4,6 @@ SET IncludeFile=..\gitVersionInfo.h
 <NUL SET /p IncludeTxt=#define GIT_VERSION_INFO '> %IncludeFile%
 FOR /f %%a IN ('git describe --abbrev^=8 --always --tags --dirty') DO <NUL SET /p IncludeTxt=%%a>> %IncludeFile%
 git describe --abbrev^=8 --always --tags --dirty > NUL
-IF %ERRORLEVEL%==0 ( ECHO '>> %IncludeFile% ) else ( ECHO Unversioned from $Format:%H$ '>> %IncludeFile% )
+IF %ERRORLEVEL%==0 ( ECHO '>> %IncludeFile% ) else ( ECHO Unversioned from $Format:%D$ '>> %IncludeFile% )
 
 EXIT /B 0
